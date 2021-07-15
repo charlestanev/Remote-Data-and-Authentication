57ab409ca38a8a0897fd6631059e0de71d4521f6
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-07-15 20:42:29 +0300

    add

--- 01.Messenger/app.js
+++ 01.Messenger/app.js
@@ -1,5 +1,51 @@
-function attachEvents() {
-    console.log('TODO...');
+let sendButton = document.getElementById('submit');
+sendButton.addEventListener('click', createMessage);
+let refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', getAllMessages);
+
+let textarea = document.getElementById('messages');
+
+
+async function getAllMessages() {
+    try {
+        let url = 'http://localhost:3030/jsonstore/messenger';
+        let getMessagesResult = await fetch(url);
+        let messages = await getMessagesResult.json();
+
+        let messagesString = Object.values(messages)
+            .map(message => `${message.author}: ${message.content}`)
+            .join('\n');
+
+        textarea.value = messagesString;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-attachEvents();
+async function createMessage() {
+    try {
+        let authorInput = document.getElementById('author');
+        let contentInput = document.getElementById('content');
+        let url = 'http://localhost:3030/jsonstore/messenger';
+
+        let newMessage = {
+            author: authorInput.value,
+            content: contentInput.value
+        };
+
+        let createResponse = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(newMessage)
+        });
+
+        let createResult = await createResponse.json();
+        console.log(createResult);
+        let createdMessageString = `${createResult.author}: ${createResult.content}`;
+        textarea.value = textarea.value + `\n${createdMessageString}`;
+    } catch (err) {
+        console.error(err);
+    }
+}
